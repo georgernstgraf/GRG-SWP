427830841376f5f6078b8fbaf15932f5151c1006
Georg Graf (SPG) <grafg@spengergasse.at>
2025-11-05 10:34:41 +0100

    commandline args verwenden

---
+++ 3ahwii/2025-11_brueche/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World als Klasse!");
+        foreach (string s in args)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
