69830dce4ff3d9c8b3343c254b9be94bbe1725ac
Georg Graf (SPG) <grafg@spengergasse.at>
2026-01-26 13:56:16 +0100

    dom + js

---
+++ 2ahwii/2026-01-19_lambda/script1.ts
@@ -0,0 +1,9 @@
+const input = [7,2,6,3,8,0];
+
+function ist_gerade(n: number): boolean {
+	console.log(`ist gerade?: ${n}`);
+	return n%2 == 0;
+}
+
+const output = input.filter(ist_gerade);
+console.log(output);
---
+++ 2ahwii/2026-01-19_lambda/sort1.ts
@@ -0,0 +1,5 @@
+const input = [7,2,6,3,8,0];
+
+const output = input.sort((a,b) => b-a).toReversed();
+console.log(input);
+console.log(output);
