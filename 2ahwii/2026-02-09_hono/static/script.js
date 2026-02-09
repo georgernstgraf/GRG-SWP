4cdb6818500cc0327a3b73521abe3e4b1c984554
Georg Graf (SPG) <grafg@spengergasse.at>
2026-02-09 13:35:45 +0100

    hono mit mathe kombiniert

---
+++ 2ahwii/2026-02-09_hono/static/script.js
@@ -0,0 +1,17 @@
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
+const stepInput = document.getElementById("step");
+const tableBody = document.getElementById("table-body");
+
+function f(x) {
+    return x * x;
+}
+function g(x) {
+    return x * x / 4;
+}
+function h(x) {
+    return x * x - 4;
+}
+function i(x) {
+    return x * x/4 - 4;
+}
